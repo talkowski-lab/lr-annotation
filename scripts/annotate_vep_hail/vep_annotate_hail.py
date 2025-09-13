f8d17a6018bcc88b528bd9571269a36f7cb57c07
Karan Jaisingh <kjaising@broadinstitute.org>
2025-09-13 16:30:09 -0400

    Updated script

--- scripts/annotate_vep_hail/vep_annotate_hail.py
+++ scripts/annotate_vep_hail/vep_annotate_hail.py
@@ -31,23 +31,55 @@ hl.init(
         "spark.executor.memory": f"{int(np.floor(mem*0.4))}g",
         "spark.driver.cores": cores,
         "spark.driver.memory": f"{int(np.floor(mem*0.4))}g",
-        # "spark.hadoop.fs.gs.requester.pays.mode": "CUSTOM",
-        # "spark.hadoop.fs.gs.requester.pays.buckets": "hail-datasets-us-central1",
-        # "spark.hadoop.fs.gs.requester.pays.project.id": gcp_project,
     }, 
     tmp_dir="tmp", 
     local_tmpdir="tmp",
 )
 
+print(f"Reading VCF: {vcf_file}")
 header = hl.get_vcf_metadata(vcf_file) 
 mt = hl.import_vcf(vcf_file, force_bgz=True, array_elements_required=False, call_fields=[], reference_genome=build)
 
+print("Running VEP...")
 mt = hl.vep(mt, config='vep_config.json', csq=False, tolerate_parse_error=True)
 mt = mt.annotate_rows(info = mt.info.annotate(vep=hl.json(mt.vep)))
 mt = mt.drop('vep', 'vep_proc_id')
 
-with open('vep_config.json', 'r') as f:
-    vep_config = json.load(f)
-header['info']['vep'] = {'Description': f"Consequence annotations from Ensembl VEP. Format: {vep_config['vep_json_schema']}", 'Number': '.', 'Type': 'String'}
+print("Selecting most severe consequence...")
+most_severe_csq = hl.vep_transcript_consequences(mt.vep).most_severe()
 
+print("Flattening structure...")
+vep_struct = hl.struct(
+    transcript_consequences=hl.array([
+        hl.struct(
+            feature_type=most_severe_csq.biotype,
+            feature_id=most_severe_csq.transcript_id,
+            consequence_terms=most_severe_csq.consequence_terms,
+            impact=most_severe_csq.impact,
+            gene_id=most_severe_csq.gene_id,
+            gene_symbol=most_severe_csq.gene_symbol,
+            canonical=most_severe_csq.canonical,
+            polyphen_prediction=most_severe_csq.polyphen_prediction,
+            sift_prediction=most_severe_csq.sift_prediction,
+            hgvsc=most_severe_csq.hgvsc,
+            hgvsp=most_severe_csq.hgvsp
+        )
+    ]),
+    most_severe_consequence=mt.vep.most_severe_consequence,
+    variant_class=mt.vep.variant_class,
+)
+
+print("Annotating JSON string onto main matrix table...")
+mt = mt.annotate_rows(info=mt.info.annotate(vep=hl.json(vep_struct)))
+mt = mt.drop('vep', 'vep_proc_id')
+
+print("Updating VCF header...")
+vep_format_string = "|".join(
+    [f"{f}" for f, t in vep_struct.transcript_consequences.dtype.element_type.items()]
+)
+header['info']['vep'] = {'Description': f'Consequence annotations from Ensembl VEP. Format: {vep_format_string}', 'Number': '.', 'Type': 'String'}
+
+print("Exporting annotated VCF...")
 hl.export_vcf(dataset=mt, output=vep_annotated_vcf_name, metadata=header)
+
+print("VEP annotation complete.")
